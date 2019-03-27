81604bb47644cf66d7a40daf2c8491c91545d912
nikithasharma <43857845+nikithasharma@users.noreply.github.com>
2019-03-27 23:09:18 +0530

    Add files via upload

---
+++ 1/password.c
@@ -0,0 +1,40 @@
+    #include <stdio.h>
+    #include <conio.h>
+    #include <string.h>
+    char word[10];
+	int i;
+	word="password";
+	void change()
+	{
+		char pass[10];
+		printf("Enter old password:\n");
+		fflush(stdin);
+		for(int i=0;i<8;i++)
+		{
+			pass[i]=getch();
+			printf("*");   	
+		}
+		pass[i]='\0';
+		if(!strcmp(pass,word))
+		{
+			printf("Enter new password:\n");
+			fflush(stdin);
+			for(int i=0;i<8;i++)
+			{
+				word[i]=getch();
+				printf("*");   	
+			}
+			word[i]='\0';
+		}
+	}
+    int login(char *name, char *pass)
+    {
+     if(!strcmp(name,"nik") && !strcmp(pass,word))
+     {
+		return 0;
+     }
+	 else
+     {
+		return 1;
+     }
+    }
